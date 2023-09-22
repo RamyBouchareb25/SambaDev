739f82e44d88bf418354989a70b821a9b740b011
Rami Leghrib <95674555+RamiLGH8@users.noreply.github.com>
2023-09-23 01:39:22 +0200

    cv

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sambadev/models/auth.dart';
+import 'package:sambadev/models/icomoon_icons.dart';
 import 'package:sambadev/widgets/widgets.dart';
 
 class Profile extends StatefulWidget {
@@ -21,31 +23,46 @@ class _ProfileState extends State<Profile> {
                         Row(
                          children: [
                            Text('Nom'),
-                           Text('Leghrib'),
+                           Text(Auth.student!.nom),
 
                          ],
                        ),
-                        Row(
-                         children: [
-                           Text('Nom'),
-                           Text('Leghrib'),
-
-                         ],
-                       ),
+                       
                           Row(
                          children: [
                            Text('Prenom'),
-                           Text('Rami'),
+                           Text(Auth.student!.prenom),
 
                          ],
                        ),
                         Row(
                          children: [
                            Text('Lieu'),
-                           Text('Ain Benian'),
+                           Text(Auth.student!.ville),
+                         ],
+                       ),
+                        Row(
+                         children: [
+                           Text('Date'),
+                           Text(Auth.student!.dateDeNaissance),
+
                          ],
                        ),                
-                       ],)],)
+                       ],)],
+                       ),
+                       Row(children: [Text('Etudes'),
+                                      Text(Auth.student!.filiere)
+                       ],),
+                       Row(children: [Text('Section'),
+                                      Text(Auth.student!.section.toString()),
+                       ],),
+                       Row(children: [Text('Groupe'),
+                                      Text(Auth.student!.groupe.toString())
+                       ],),
+                       Row(children: [Icon(Icomoon.Card)],),
+                       Row(children: [Icon(Icomoon.Marks)],),
+                       Row(children: [Icon(Icomoon.Calc)],),
+                       Row(children: [Icon(Icomoon.Bac)],),
                  ]),
                  bottomNavigationBar: bottomNavBar(selectedPage: 4, context: context),
                  ));
